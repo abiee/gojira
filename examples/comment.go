14077be4a29defb7d7c08acf68ea7f08f3ed44c1
Jay Bhat <jbhat@ooyala.com>
2014-10-23 18:14:57 -0700

    format and restructure examples

--- examples/comment.go
+++ examples/comment.go
@@ -1,15 +1,19 @@
-package main
+package gojira
 
 import (
-    "code.google.com/p/gopass"
+	"code.google.com/p/gopass"
 	"github.com/jbhat/go-jira-client"
-  )
+)
 
-func main() {
+func TestComment() {
 	pass, err := gopass.GetPass("Pass: ")
-	if err != nil {panic(err.Error())}
+	if err != nil {
+		panic(err.Error())
+	}
 	jira := gojira.NewJira("https://jira.corp.ooyala.com", "/rest/api/latest", "/activity", &gojira.Auth{"jbhat", pass})
 	issue, err := jira.Issue("APPSPLAT-840")
-    err = jira.AddComment(&issue, "test jira plugin")
-    if err != nil {panic(err.Error())}
+	err = jira.AddComment(&issue, "test jira plugin")
+	if err != nil {
+		panic(err.Error())
+	}
 }
--- examples/user.go
+++ examples/user.go
@@ -1,25 +1,25 @@
-package main
+package gojira
 
 import (
-	"os"
 	"flag"
 	"fmt"
-	"time"
-	"strconv"
 	"github.com/plouc/go-jira-client"
 	"io/ioutil"
 	"launchpad.net/goyaml"
+	"os"
+	"strconv"
+	"time"
 )
 
 type Config struct {
-    Host         string `yaml:"host"`
-    ApiPath      string `yaml:"api_path"`
-    ActivityPath string `yaml:"activity_path"`
-    Login        string `yaml:"login"`
-    Password     string `yaml:"password"`
+	Host         string `yaml:"host"`
+	ApiPath      string `yaml:"api_path"`
+	ActivityPath string `yaml:"activity_path"`
+	Login        string `yaml:"login"`
+	Password     string `yaml:"password"`
 }
 
-func main() {
+func TestUser() {
 	startedAt := time.Now()
 	defer func() {
 		fmt.Printf("processed in %v\n", time.Now().Sub(startedAt))
@@ -27,30 +27,30 @@ func main() {
 
 	help := flag.Bool("help", false, "Show usage")
 
-    // read config file
-    file, e := ioutil.ReadFile("config.yml")
-    if e != nil {
-        fmt.Printf("Config file error: %v\n", e)
-        os.Exit(1)
-    }
+	// read config file
+	file, e := ioutil.ReadFile("config.yml")
+	if e != nil {
+		fmt.Printf("Config file error: %v\n", e)
+		os.Exit(1)
+	}
 
-    // parse config file
-    config := new(Config)
-    err := goyaml.Unmarshal(file, &config)
-    if err != nil {
-        panic(err)
-    }
+	// parse config file
+	config := new(Config)
+	err := goyaml.Unmarshal(file, &config)
+	if err != nil {
+		panic(err)
+	}
 
-    jira := gojira.NewJira(
-        config.Host,
-        config.ApiPath,
-        config.ActivityPath,
-        &gojira.Auth{config.Login, config.Password,},
-    )
+	jira := gojira.NewJira(
+		config.Host,
+		config.ApiPath,
+		config.ActivityPath,
+		&gojira.Auth{config.Login, config.Password},
+	)
 
 	var method string
-	flag.StringVar(&method, "m", "", "Specify method to retrieve user(s) data, available methods:\n" +
-									 "  > -m user -u USERNAME")
+	flag.StringVar(&method, "m", "", "Specify method to retrieve user(s) data, available methods:\n"+
+		"  > -m user -u USERNAME")
 
 	var username string
 	flag.StringVar(&username, "u", "", "Specify username")
@@ -82,10 +82,10 @@ func main() {
 		format := "> %-14s: %s\n"
 
 		fmt.Printf("%s\n", user.Name)
-		fmt.Printf(format, "self",          user.Self)
+		fmt.Printf(format, "self", user.Self)
 		fmt.Printf(format, "email address", user.EmailAddress)
-		fmt.Printf(format, "display name",  user.DisplayName)
-		fmt.Printf(format, "active",        strconv.FormatBool(user.Active))
-		fmt.Printf(format, "time zone",     user.TimeZone)
+		fmt.Printf(format, "display name", user.DisplayName)
+		fmt.Printf(format, "active", strconv.FormatBool(user.Active))
+		fmt.Printf(format, "time zone", user.TimeZone)
 	}
 }
